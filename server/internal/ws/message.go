6163397099dcf220a2a8fc2043c5f6e0b7b4a628
ayushrathour <ayushrathour2910@gmail.com>
2025-11-05 09:56:09 +0530

    Add Frontend

--- server/internal/ws/message.go
+++ server/internal/ws/message.go
@@ -3,14 +3,13 @@ package ws
 type InboundMessage struct {
 	SenderID string                 `json:"sender_id"`
 	ReceiverID string               `json:"receiver_id"`
-	Content string 			  `json:"content"`
-	// Type string                 `json:"type"`
-	// Data map[string]interface{} `json:"data"`
+	Content string 			  		`json:"content"`
+
 }
 
 type OutboundMessage struct {
 	SenderID string                 `json:"sender_id"`
 	ReceiverID string               `json:"receiver_id"`
-	Content string 			  `json:"content"`
+	Content string 			  		`json:"content"`
 }
 
